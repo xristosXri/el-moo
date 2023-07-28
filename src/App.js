c6b38f6d41a7c0f18232a1e95de5df8cfd7c04ec
xristosXri <xristos.xri@gmail.com>
2023-07-28 21:16:38 +0300

    new project

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+
+  let audio = new Audio("/moo.wav")
+
+  const start = () => {
+    audio.play()
+  }
+
+
+	return (
+		<div>
+			<nav class="navbar background">
+				<div class ="elmoo">
+          <button class="btn btn-sm" onClick={start}>El Moo</button>
+        </div>
+
+				
+			</nav>
+
+			<section class="section">
+				<div class="box-main">
+					<div class="firstHalf">
+						<h1 class="text-big">
+							Dena's Wedding
+						</h1>
+						<p class="text-small">
+							Hi I'm Dena.  In my spare time I pretend to sing and dance.
+              I like cows very much.
+              Also, I am getting married.
+						</p>
+					</div>
+				</div>
+			</section>			
+			<footer className="footer">
+				<p className="text-footer">
+					Copyright © Her family
+				</p>
+			</footer>
+		</div>
+	)
+}
+
+export default App
